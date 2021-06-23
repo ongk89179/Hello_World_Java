e563e6968e42b9e2328665ee300073c2511b05c1
Kevin Ong <ongk89179@gmail.com>
2021-06-23 11:01:41 -0700

    Updated Java file to Hello World covered in class to fix it correctly

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld{
-    public void main(String[] args){
+    //The Main Method! 
+    public static void main(String[] args) {
         System.out.println("Hello World");
     }
 }
